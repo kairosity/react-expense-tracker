4eb510b3b0c4c72fdf019f58206f5f6a9855e25a
Karina Finegan <kairosity@users.noreply.github.com>
2021-09-04 15:17:33 +0100

    added two-way binding and various useState hooks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -5,13 +7,19 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () =>{
+        setTitle('Biscuits!');
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">€{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
         );
 }
